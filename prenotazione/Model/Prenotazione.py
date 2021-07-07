fb843de12d3f809b76f867b9bb99bc95289a0583
Aldo Paganica <s1092434@studenti.univpm.it>
2021-07-07 15:31:15 +0200

    model prenotazione

---
+++ prenotazione/Model/Prenotazione.py
@@ -0,0 +1,9 @@
+import json
+
+
+class Prenotazione():
+    lista_tavoli = json.load(open("listatavoli.json"))
+         def __init__(self, lista_tavoli, orario):
+            super(Prenotazione, self).__init__()
+            self.lista_tavoli = lista_tavoli
+            self.orario = orario
