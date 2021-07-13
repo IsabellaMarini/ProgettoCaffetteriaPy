744ae2d632f31ebbfc69fb6440c5437d09419e7e
IsabellaMarini <76102845+IsabellaMarini@users.noreply.github.com>
2021-07-13 10:36:21 +0200

    Dipendente

--- ListaDipendenti/ListaDipendenti.py
+++ ListaDipendenti/ListaDipendenti.py
@@ -1,9 +1,27 @@
 from json
 from Dipendente.Model import Dipendente
 lD=json.load(open("ListaDipendenti.json"))
+
 class ListaDipendenti():
     def __init__(self):
         super(ListaDipendenti, self).__init__()
         self.lD.append(ListaDipendenti)
+
+    def getListaDipendente(self):
+        return self.lD
+
+    def aggiungiDipendente(self, Dipendente):
+        for dipendenteDaAggiungere in lD:
+            self.lD.append(Dipendente( dipendenteDaAggiungere["nome"],
+                                       dipendenteDaAggiungere["cognome"],
+                                       dipendenteDaAggiungere["numero"],
+                                       dipendenteDaAggiungere["email"],
+                                       dipendenteDaAggiungere["password"]
+                                     ))
+
+    def eliminaDipendente(self, Dipendente):
+        self.lD.remove(Dipendente)
+
+
 
 
