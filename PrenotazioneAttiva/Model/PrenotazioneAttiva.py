4033ad19b9e16285e5018d93b090163493ad7b1c
Aldo Paganica <s1092434@studenti.univpm.it>
2021-07-17 11:02:29 +0200

    prenotazione attiva

--- PrenotazioneAttiva/Model/PrenotazioneAttiva.py
+++ PrenotazioneAttiva/Model/PrenotazioneAttiva.py
@@ -1,6 +1,18 @@
-from json
+
+from Login.Model.Login import Login
+from prenotazione.Model.Prenotazione import Prenotazione
+
 
 class PrenotazioneAttiva():
 
     def __init__(self):
+        self.login = Login
+        self.prenotazione = Prenotazione
+
+    def show_prenotazione(self):
+        if (self.login.role == "Cliente") and (self.login.email == Login.email):
+            return self.prenotazione
+        else:
+            print("Non ci sono prenotazioni attive!")
+
 
